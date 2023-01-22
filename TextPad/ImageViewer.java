a74ad2319721ab3cbffea171438da022597a2db8
Peter Adi Saputro <73358706+peteronprogramming@users.noreply.github.com>
2023-01-22 17:51:00 +0700

    Create ImageViewer.java

---
+++ TextPad/ImageViewer.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+public class ImageViewer{
+	public static void main(String[] args){
+		JFrame frame = new ImageViewerFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
+
+class ImageViewerFrame extends JFrame{
+	public ImageViewerFrame(){
+		setTitle("ImageViewer");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+		// use label to display images
+		label = new JLabel();
+		add(label);
+
+		// set up file chooser
+		chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new File("."));
+
+		// set up menu bar
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu menu = new JMenu("File");
+		menuBar.add(menu);
+
+		JMenuItem openItem = new JMenuItem("Open");
+		menu.add(openItem);
+		openItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				//show file chooser dialog
+				int result = chooser.showOpenDialog(null);
+
+				//if file is selected, set it as an icon of label
+				if (result == JFileChooser.APPROVE_OPTION){
+					String name = chooser.getSelectedFile().getPath();
+					label.setIcon(new ImageIcon(name));
+				}
+			}
+		});
+
+		JMenuItem exitItem = new JMenuItem("Exit");
+		menu.add(exitItem);
+		exitItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				System.exit(0);
+			}
+		});
+	}
+
+	private JLabel label;
+	private JFileChooser chooser;
+	private static final int DEFAULT_WIDTH = 300;
+	private static final int DEFAULT_HEIGHT = 400;
+}
