5747531160662cefa50c9ffa4d8fd293ccbf85eb
Peter Adi Saputro <73358706+peteronprogramming@users.noreply.github.com>
2024-10-10 07:27:21 +0700

    Create Employee.java

---
+++ TextPad/Peter/PackageTest/Employee.java
@@ -0,0 +1,44 @@
+package PackageTest;
+/* the classes in this file are part of this package
+   package name is not case-sensitive, may be written
+   differently from the directory name
+*/
+
+// import statement always come after package statement
+import java.util.*;
+
+public class Employee
+{
+   public Employee(String n, double s, int year, int month, int day)
+   {
+      name = n;
+      salary = s;
+      GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+      hireDay = calendar.getTime();
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public double getSalary()
+   {
+      return salary;
+   }
+
+   public Date getHireDay()
+   {
+      return hireDay;
+   }
+
+   public void raiseSalary(double byPercent)
+   {
+      double raise = salary * byPercent / 100;
+      salary += raise;
+   }
+
+   private String name;
+   private double salary;
+   private Date hireDay;
+}
